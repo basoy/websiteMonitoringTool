89b67f2e99e79102e1d7f5e4c4098599cf7dbe01
Serg_Bash <basoy1988@gmail.com>
2019-02-21 17:46:26 +0200

    refactored names of methods and added validation

--- Server/src/main/java/com/bash/serg/monitor/entity/impl/Url.java
+++ Server/src/main/java/com/bash/serg/monitor/entity/impl/Url.java
@@ -20,27 +20,17 @@ public class Url extends BaseEntity {
     @NotBlank(message = "url is mandatory")
     private String url;
 
-    @Digits(integer = 100000, fraction = 0)
-    @NotNull
     private int periodMonitoring;
 
-    @Digits(integer = 100000, fraction = 0)
-    @NotNull
     private int responseTime;
 
-    @Digits(integer = 100000, fraction = 0)
-    @NotNull
     private int responseCode;
 
     private Status status;
 
     private String subQuery;
 
-    @Digits(integer = 100000, fraction = 0)
-    @NotNull
     private long minResponseSize;
 
-    @Digits(integer = 100000, fraction = 0)
-    @NotNull
     private long maxResponseSize;
 }
